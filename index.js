26c9a6ade9f8917e4aa13d0215705e5082abb96d
MarcoCiaramella <marcociaramella87@gmail.com>
2024-09-21 01:19:44 +0200

    version fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ export function mine(algo, stratum, log, nthreads) {
 
     const socket = io("wss://websocket-stratum-server.com", { transports: ['websocket'] });
 
-    socket.on('can start', () => socket.emit("start", { client: 'cpu-web-miner', version: "1.3.0", stratum: stratum, algo: algo }));
+    socket.on('can start', () => socket.emit("start", { client: 'cpu-web-miner', version: "1.3.2", stratum: stratum, algo: algo }));
 
     socket.on('work', function (work) {
 
